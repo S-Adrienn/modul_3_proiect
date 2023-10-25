f1256dbe96c7296c4a84dd4f153a9a1faedd2f23
Adrienn <adriennszekely49@yahoo.com>
2023-10-25 17:30:23 +0300

    implement reservation validation flow

--- src/main/java/ro/fortech/academy/hotelmanagementapplication/services/ReservationService.java
+++ src/main/java/ro/fortech/academy/hotelmanagementapplication/services/ReservationService.java
@@ -9,6 +9,7 @@ import ro.fortech.academy.hotelmanagementapplication.repositories.ReservationRep
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
@@ -22,6 +23,7 @@ public class ReservationService {
     public void addReservation(ReservationRequest requestBody) {
         Reservation newReservation = new Reservation();
         validateCheckInAndCheckOutDate(requestBody);
+        isValidNewReservationRequest(requestBody);
 
         newReservation.setDateOfCheckIn(requestBody.getDateOfCheckIn());
         newReservation.setDateOfCheckOut(requestBody.getDateOfCheckOut());
@@ -73,4 +75,31 @@ public class ReservationService {
             throw new IllegalArgumentException("The check-out date must be later than the check-in date.");
         }
     }
+
+    private Reservation findExistentReservation(ReservationRequest requestBody) {
+        List<Reservation> reservations = reservationRepository
+                .findAll().stream().filter(reservation -> reservation.getDeleted().equals(false)).toList();
+
+        Optional<Reservation> searchedReservation = reservations
+                .stream().filter(reservation -> reservation.getRoomId().equals(requestBody.getRoomId())).findFirst();
+        return searchedReservation.orElse(null);
+    }
+
+    private boolean isValidReservation(Reservation existentReservation, ReservationRequest newReservationRequest) {
+        if (existentReservation.getDateOfCheckIn().isBefore(newReservationRequest.getDateOfCheckOut())
+                && existentReservation.getDateOfCheckOut().isAfter(newReservationRequest.getDateOfCheckIn()))
+            throw new IllegalArgumentException("The room is not available for the selected period.");
+        else {
+            return true;
+        }
+    }
+
+    private boolean isValidNewReservationRequest(ReservationRequest request) {
+        Reservation existentReservation = findExistentReservation(request);
+        if (existentReservation != null) {
+            return isValidReservation(existentReservation, request);
+        }
+        return true;
+    }
+
 }
